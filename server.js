8f4761c11ef4378dc4b9f01d47f8aa3fb2bc3c45
Jonathan Abantao <jonabantao@gmail.com>
2018-04-30 14:14:32 -0700

    Refactor endpoints to seperate route files

--- server.js
+++ server.js
@@ -1,55 +1,12 @@
 const express = require('express');
-const moment = require('moment');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/api/time/:timeString', (req, res) => {
-  const timeString = req.params.timeString;
-  let time;
-  let unix;
-  let natural;
+const timestamp = require('./routes/timestamp');
+const whoAmI = require('./routes/whoami');
 
-  if (Number.isNaN(Number(timeString))) {
-    time = moment(timeString, 'MMMM Do YYYY');
-    unix = time.unix();
-    natural = time.format('MMMM Do YYYY');
-  } else {
-    time = moment.unix(timeString);
-    unix = time;
-    natural = time.format('MMMM Do YYYY');
-  }
-
-  if (!time.isValid()) {
-    return res.status(422).json({
-      unix: null,
-      natural: null,
-    });
-  }
-
-  res.status(200).json({
-    unix,
-    natural,
-  });
-});
-
-app.get('/api/whoami', (req, res) => {
-  const ipaddress = req.header('x-forwarded-for') ||
-    req.connection.remoteAddress;
-  let software = req.headers['user-agent'].match(/\(([^)]+)\)/);
-  let language = req.headers['accept-language'].match(/^([^,]*)/);
-
-  // Checks just in case regex fails and returns null
-  software = software ? software[1] : 'Not available';
-  language = language ? language[1] : 'Not available';
-
-  const payload = {
-    ipaddress,
-    language,
-    software
-  };
-
-  res.status(200).json(payload);
-});
+app.use('/api/time', timestamp);
+app.use('/api/whoami', whoAmI);
 
 app.get('*', (req, res) => {
   res.status(404).json({ message: 'Not a valid API endpoint'});
